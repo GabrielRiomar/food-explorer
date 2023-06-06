95946e5ee4dd6333f98492ddb7ccc9962a850055
Gabriel Riomar Lopes <gabrielriomar@gmail.com>
2023-06-06 13:58:16 -0300

    removed component input and added a new one

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
 export const Container = styled.header`
-  grid-area: header;
+  /* grid-area: header; */
   height: 13.68rem;
   width: 100%;
   background-color: ${({ theme }) => theme.COLORS.BACKGROUND_HEADER};
@@ -10,28 +10,27 @@ export const Container = styled.header`
 
   display: flex;
   align-items: center;
-  justify-content: space-between;
-
   gap: 3.2rem;
-  padding: 0 12.3rem;
-  position: relative;
+  padding: 2.8rem 12.3rem;
+  margin: 0 auto;
 `
 
 export const Brand = styled.div`
   display: flex;
-  justify-content: center;
   align-items: center;
-  gap: 1rem;
+  gap: 1.2rem;
   animation: fadeIn 0.5s;
 
-  color: ${({ theme }) => theme.COLORS.PINK};
+  color: ${({ theme }) => theme.COLORS.WITHE_200};
+
   font-size: 2.4rem;
   font-weight: 700;
+  line-height: 3rem;
 
   > svg {
     animation: fade 5s infinite;
-    width: 7rem;
-    height: 7rem;
+    width: 5rem;
+    height: 5rem;
   }
 
   @keyframes fade {
@@ -58,8 +57,36 @@ export const Brand = styled.div`
 `
 
 export const Search = styled.div`
+  width: 40rem;
   display: flex;
   align-items: center;
+
   padding: 0 6.4rem;
+  border-radius: 0.8rem;
   flex-grow: 1;
+
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_INPUT};
+  color: ${({ theme }) => theme.COLORS.WHITE_100};
+
+  > input {
+    width: 100%;
+    display: flex;
+    height: 5.6rem;
+    font-size: 1.6rem;
+
+    /* padding: 1.9rem; */
+    color: ${({ theme }) => theme.COLORS.WHITE_100};
+    background: transparent;
+    border: none;
+    /* text-align: center; */
+
+    ::placeholder {
+      color: ${({ theme }) => theme.COLORS.GRAY_300};
+      padding: 1.6rem 1.4rem;
+      font-size: 1.6rem;
+    }
+  }
+  > svg {
+    margin-left: 1.4rem;
+  }
 `
