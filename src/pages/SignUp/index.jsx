754fe3b5e6ffe685a3081070e2c21ec912cb4e16
Gabriel Riomar Lopes <gabrielriomar@gmail.com>
2023-06-08 18:41:09 -0300

    minor adjusts

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,14 +1,40 @@
+import { useState } from "react"
 import { Container, Form } from "./styles"
 
-import { Link } from 'react-router-dom'
-
-// import {  FiMail, FiLogIn, FiLock } from "react-icons/fi"
+import{api} from '../../services/api'
+import { Link, useNavigate } from 'react-router-dom'
 
 import { Input } from "../../components/Input"
 
 import { Button } from "../../components/Button"
 
 export function SignUp(){
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const navigate = useNavigate()
+
+  function handleSignUp(){
+    console.log(name, email, password)
+    if(!name || !email || !password){
+      return alert('Fill all the fields!')
+    }
+
+    api.post('/users', { name, email, password})
+    .then(() => {
+      alert('User successfully created')
+      navigate('/')
+    })
+    .catch(error => {
+      if(error.response){
+        alert(error.response.data.message)
+      }else{
+        alert('Failed to create user')
+      }
+    })
+  }
+
   return(
     <Container>
       <div>
@@ -25,21 +51,27 @@ export function SignUp(){
         <h2>Create your account</h2>
         
         <Input
-          placeholder="Name"
+          placeholder="Username"
           type="text"
+          onChange={e => setName(e.target.value)}
         />
 
         <Input
           placeholder="email@email.com"
           type="text"
+          onChange={e => setEmail(e.target.value)}
         />
 
         <Input
           placeholder="Password"
           type="password"
+          onChange={e => setPassword(e.target.value)}
         />
 
-        <Button title="Create"/>
+        <Button 
+          title="Create"
+          onClick={handleSignUp}
+        />
 
         <Link to="/">Back to Log In</Link>
 
