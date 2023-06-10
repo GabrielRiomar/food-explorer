90252ac811756154af0ed51de4dd52a61d31a6cc
Gabriel Riomar Lopes <gabrielriomar@gmail.com>
2023-06-10 13:32:44 -0300

    added textarea component

---
+++ src/components/TextArea/styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const Container = styled.textarea`
+  width: 100%;
+  height: 17rem;
+
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_INPUT};
+  color: ${({ theme }) => theme.COLORS.WHITE_100};
+
+  margin-bottom: 3.2rem;
+  border-radius: 0.8rem;
+  padding: 1.4rem;
+
+  border: none;
+  resize: none;
+
+  &::placeholder {
+    color: ${({ theme }) => theme.COLORS.GRAY_300};
+  }
+`
